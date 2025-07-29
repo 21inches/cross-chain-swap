791d96da9d49e2df9205eb01c3aca02e840e58c2
xana-rahmani <xanarahmani@gmail.com>
2025-07-30 03:00:39 +0330

    Set USDT as feeTokenAddress

--- tronbox/migrations/2_deploy_escrow_factory.js
+++ tronbox/migrations/2_deploy_escrow_factory.js
@@ -8,7 +8,7 @@ module.exports = function (deployer, network, accounts) {
     const lopAddress = "TAYjAyuKjKvkhkcvgJ7CgrJ8PVziU5vr4R";
 
     // 2. Fee Token Address (Using USDT for Nile)
-    const feeTokenAddress = "TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf"; // TODO
+    const feeTokenAddress = "TUrzzZSyCtu2aXdkxnwUqNpqBgoodrbN8Y";
 
     // 3. Access Token Address
     const accessTokenAddress = "TUdoqfVVJviAdDYMgJPSnGbfVo64eyq2D1";
