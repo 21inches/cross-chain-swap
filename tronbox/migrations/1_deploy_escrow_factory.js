e0e7fd32baadeccdd7ff545db9e42f4f01edaced
xana-rahmani <xanarahmani@gmail.com>
2025-07-30 04:07:06 +0330

    set configs

--- tronbox/migrations/1_deploy_escrow_factory.js
+++ tronbox/migrations/1_deploy_escrow_factory.js
@@ -1,29 +1,35 @@
 const EscrowFactory = artifacts.require("EscrowFactory");
 
+const configs = {
+    tron: {
+        lop: "",
+        feeToken: "",
+        accessToken: ""
+    },
+    nile: {
+        lop: "TAYjAyuKjKvkhkcvgJ7CgrJ8PVziU5vr4R",
+        feeToken: "TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf", // USDT on Nile
+        accessToken: "TUdoqfVVJviAdDYMgJPSnGbfVo64eyq2D1"
+
+    },
+};
+
 module.exports = function (deployer, network, accounts) {
     console.log("Deploying Escrow...");
     console.log("Network:", network);
     console.log("Accounts:", accounts[0]);
 
-    const lopAddress = "TAYjAyuKjKvkhkcvgJ7CgrJ8PVziU5vr4R";
-
-    // 2. Fee Token Address (Using USDT for Nile)
-    const feeTokenAddress = "TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf";
-
-    // 3. Access Token Address
-    const accessTokenAddress = "TUdoqfVVJviAdDYMgJPSnGbfVo64eyq2D1";
-
     // 4. Fee Bank Owner
-    const feeBankOwnerAddress = accounts; // TODO
+    const feeBankOwnerAddress = accounts;
 
     // 5. & 6. Rescue Delay (8 days in seconds)
     const rescueDelay = 691200;
 
     console.log("----------------------------------------------------");
     console.log("Deploying EscrowFactory with the following arguments:");
-    console.log(`  1. LOP:             ${lopAddress}`);
-    console.log(`  2. Fee Token:       ${feeTokenAddress}`);
-    console.log(`  3. Access Token:    ${accessTokenAddress}`);
+    console.log(`  1. LOP:             ${configs[network].lopAddress}`);
+    console.log(`  2. Fee Token:       ${configs[network].feeTokenAddress}`);
+    console.log(`  3. Access Token:    ${configs[network].accessTokenAddress}`);
     console.log(`  4. Fee Bank Owner:  ${feeBankOwnerAddress}`);
     console.log(`  5. Rescue Delay 1:  ${rescueDelay}`);
     console.log(`  6. Rescue Delay 2:  ${rescueDelay}`);
@@ -32,9 +38,9 @@ module.exports = function (deployer, network, accounts) {
     // Deploy the contract, passing all arguments in the correct order.
     deployer.deploy(
         EscrowFactory,
-        lopAddress,
-        feeTokenAddress,
-        accessTokenAddress,
+        configs[network].lopAddress,
+        configs[network].feeTokenAddress,
+        configs[network].accessTokenAddress,
         feeBankOwnerAddress,
         rescueDelay,
         rescueDelay
